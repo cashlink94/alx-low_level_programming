539639f2e68059a3aaef2ad95b05cca60fe0cad4
cashlink94 <cashlink94@gmail.com>
2023-03-16 14:15:38 -0700

    print a line

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A c program that prints a given line
+ *
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
