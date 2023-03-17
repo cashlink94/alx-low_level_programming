b1d2f0af7d1279574dcee0e61ac7f43c1b93b082
cashlink94 <cashlink94@gmail.com>
2023-03-17 03:45:55 -0700

    cool

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - A c program that prints a given line
  *
- * Return: 0(Success)
+ * Return: 1(Success)
  */
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	return (1);
 }
