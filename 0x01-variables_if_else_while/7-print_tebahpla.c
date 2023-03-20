f0f4fbfda96753e175981082e0c0d8ad42758e3c
cashlink94 <cashlink94@gmail.com>
2023-03-20 14:36:20 -0700

    print_tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
  * main - Entry - point
- *
  * Return: Always 0 (success)
  */
 
@@ -10,10 +9,11 @@ int main(void)
 	char c;
 
 	c = 'z';
-	while
-	        (c >= 'a'){
-			putchar(c);
-			c--;
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
 	putchar('\n');
 	return (0);
 }
