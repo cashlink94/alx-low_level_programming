c80f4c0f78d9226bad11b728a3f919ff24384de4
cashlink94 <cashlink94@gmail.com>
2023-03-20 14:51:53 -0700

    print_base

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- *main - Entry point
- *
+ * main - print all number base of 16 lowercases
+ * Return: 0
  */
 
 int main(void)
@@ -9,18 +9,20 @@ int main(void)
 	char c;
 
 	int d;
+
 	c = 'a';
 	d = 0;
-	while
-		(d < 10){
-			putchar(d + '0');
-			d++;
-		}
-	while
-		(c <= 'f'){
-			putchat(c);
-			c++;
-		}
+
+	while (d < 10)
+	{
+		putchar(d + '0');
+		d++;
+	}
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
 	putchar('\n');
 	return (0);
 }
