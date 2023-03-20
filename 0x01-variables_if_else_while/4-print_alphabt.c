b6092e18c14d0aa2ae1df0055f2f94504a34039a
cashlink94 <cashlink94@gmail.com>
2023-03-20 14:08:40 -0700

    printalphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
  * main -Entry point
- *
  * Return: Always 0 (success)
  */
 
@@ -10,12 +9,12 @@ int main(void)
 	char c;
 
 	c = 'a';
-	while
-		(c <= 'z'){
-			if ((c != 'q' && c != 'e') && c <= 'z')
-				outchar(c);
-			c++;
-		}
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+		putchar(c);
+		c++;
+	}
 	putchar('\n');
 	return (0);
 }
