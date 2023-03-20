94121c10de65bb4f2902aae0dd708696b8cc532b
cashlink94 <cashlink94@gmail.com>
2023-03-20 14:21:47 -0700

    print number

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
 	int a;
-        for (a = 0;  a < 10; a++)
-		putchar(a);
-	putchar('\n');
+
+	for (a = 0;  a < 10; a++)
+		printf("%d", a);
+	printf("\n");
 	return (0);
 }
