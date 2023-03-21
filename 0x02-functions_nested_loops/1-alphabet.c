4388ebc73e60cf96de2ae72d316fc4692c267953
cashlink94 <cashlink94@gmail.com>
2023-03-21 07:12:41 -0700

    number

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,10 @@
 
 void print_alphabet(void)
 {
-	char letter;
+	char la;
 
-	for (letter = 'a'; letter <= 'Z'; letter++)
-		_putchar(letter);
+	for (la = 'a'; la <= 'Z'; la++)
+		_putchar(la);
 
 	_putchar('\n');
 }
