8355e98a118aa25027d23821756e2b1ca7bb5bb6
cashlink94 <cashlink94@gmail.com>
2023-03-22 10:36:03 -0700

    main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main."
+
+/**
+ * main - tests function that prints if intriger is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = a;
+	positive_or_negative(i);
+
+	return (0);
+}
