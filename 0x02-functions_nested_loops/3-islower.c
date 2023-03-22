9c916a8f924dea8a64164d2c53d05291220aee5b
cashlink94 <cashlink94@gmail.com>
2023-03-22 08:21:20 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,10 +5,10 @@
  * @C: the character
  * Return: 1 if letter is lowercase, 0 if not
  */
-int _islower(int c)
+int _islower(int C)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
-	else 
+	else
 		return (0);
 }
