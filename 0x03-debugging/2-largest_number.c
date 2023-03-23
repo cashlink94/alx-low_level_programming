889eb2e6f845744222a520a7298c920a78a92086
cashlink94 <cashlink94@gmail.com>
2023-03-23 12:26:20 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,22 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
-	if (a > b)
-	{
-		largest = a;
-		else if (a > c)
-			largest = a;
-		else
-			largest = c;
+    int largest;
 
-	}
-	else
-	{
-		if (b < c)
-			largest = c;
-		else
-			largest = b;
-	}
-	return (largest);
+    if (a > b && a > c)
+    {
+        largest = a;
+    }
+    else if (b > c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
+
+    return (largest);
 }
