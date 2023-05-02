bd93b9813f40ed1bdb7ae5058659e6652796415d
cashlink94 <cashlink94@gmail.com>
2023-05-02 08:38:38 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * free_listint2 - frees a linked list in this P
- * @head: pointer to the listint_t list to be freed in this assigment 
+ * free_listint2 - frees a link list
+ * @head: pointer to the listint_t list to be freed
  */
 void free_listint2(listint_t **head)
 {
