0e090b1bb34a992513f1a06748a855431913b6e8
cashlink94 <cashlink94@gmail.com>
2023-05-02 02:50:48 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-add_nodeint.c 0-print_listint.c -o c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,11 +3,11 @@
 /**
  * add_nodeint - add node at the start of a linked list in this project
  * @head: pointer to the first node in the list you are working on 
- * @m: data to insert in that new node
+ * @n: data to insert in that new node on this task
  *
  * Return: pointer to the new node, or NULL if it fails
  */
-listint_t *add_nodeint(listint_t **head, const int m)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
@@ -15,7 +15,7 @@ listint_t *add_nodeint(listint_t **head, const int m)
 	if (!new)
 		return (NULL);
 
-	new->m = m;
+	new->n = n;
 	new->next = *head;
 	*head = new;
 
